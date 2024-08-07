875542616d44869097122c6f4bd1abe94de248aa
PokeyMystery <120627091+PokeyMystery@users.noreply.github.com>
2024-08-07 09:58:46 +0530

    Create package.sv

---
+++ package.sv
@@ -0,0 +1,14 @@
+`define FIFO_DEPTH 12
+`define FIFO_WIDTH 8
+   
+`include "interface.sv"
+`include "seq_item.sv"
+`include "sequence.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "agent.sv"
+`include "scoreboard.sv"
+`include "env.sv"
+`include "rst_seq.sv"
+`include "fifo_full_seq.sv"
+`include "fifo_empty_seq.sv"
